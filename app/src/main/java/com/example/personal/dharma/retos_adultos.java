d5d3acbd1c5866d26cf846516e16bfcc16c90213
ronaldvenegasp <ronaldvenegasp@gmail.com>
2016-08-18 23:57:58 -0500

    Pantallas de retos para niños y para adultos ya listas

--- app/src/main/java/com/example/personal/dharma/retos_adultos.java
+++ app/src/main/java/com/example/personal/dharma/retos_adultos.java
@@ -19,5 +19,37 @@ public class retos_adultos extends AppCompatActivity {
                 startActivity(new Intent(retos_adultos.this, Ayuda.class));
             }
         });
+
+        //Al tocar sobre el botón de Menú Principal, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.menu_principal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_adultos.this, MenuPrincipal.class));
+            }
+        });
+
+        //Al tocar sobre el botón de Sopa de letras, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.sopa_letras).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_adultos.this, MenuPrincipal.class));
+            }
+        });
+
+        //Al tocar sobre el botón de Cultura Hindú, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.cultura_hindu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_adultos.this, MenuPrincipal.class));
+            }
+        });
+
+        //Al tocar sobre el botón de Adivinanzas, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.adivinanzas).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_adultos.this, MenuPrincipal.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/personal/dharma/retos_ninos.java
+++ app/src/main/java/com/example/personal/dharma/retos_ninos.java
@@ -19,5 +19,29 @@ public class retos_ninos extends AppCompatActivity {
                 startActivity(new Intent(retos_ninos.this, Ayuda.class));
             }
         });
+
+        //Al tocar sobre el botón de Menú Principal, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.menu_principal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_ninos.this, MenuPrincipal.class));
+            }
+        });
+
+        //Al tocar sobre el botón de Cultura Hindú, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.cultura_hindu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_ninos.this, MenuPrincipal.class));
+            }
+        });
+
+        //Al tocar sobre el botón de Reto, se pasa a la actividad menu_principal de la aplicación
+        findViewById(R.id.reto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(retos_ninos.this, MenuPrincipal.class));
+            }
+        });
     }
 }
